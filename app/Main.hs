8bed35773c101d8a6f6747837ea21ff426c3c6a5
dawkrish <krishnansh710@gmail.com>
2024-11-03 16:48:25 +0530

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,22 @@
 
 module Main (main) where
 
-import Network.Simple.TCP (serve, HostPreference(HostAny), Socket, closeSock, send, recv)
-import Network.Socket (socket)
-
+import qualified Network.Simple.TCP as TCP
 
 main :: IO ()
 main = do
     let port = "6379"
     putStrLn $ "Redis server listening on port " ++ port
-    serve HostAny port $ \(socket, address) -> do
+    TCP.serve TCP.HostAny port $ \(socket, address) -> do
         putStrLn $ "successfully connected client: " ++ show address
         loop socket
 
-loop :: Socket -> IO()
+loop :: TCP.Socket -> IO()
 loop sock = do
-    msg <- recv sock 128
+    msg <- TCP.recv sock 1024
     case msg of
         Nothing -> putStrLn "Client has exited!"
         (Just bs) -> do
-            send sock "+PONG\r\n"
+            TCP.send sock "+PONG\r\n"
             loop sock
             
